4bb510205b32285930a94bb217706c32dddd1b70
yoe <128493923+NoahVanArray@users.noreply.github.com>
2025-02-12 17:09:05 +0800

    Initializing...

---
+++ visualBasic-ProgrammingLanguages-SirTan-BSCS2A/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(151), CByte(171), CByte(70))
+        Panel1.ForeColor = SystemColors.ControlText
+        Panel1.Location = New Point(73, 70)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(299, 82)
+        Panel1.TabIndex = 0
+        ' 
+        ' MainCalc
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(37), CByte(38), CByte(38))
+        ClientSize = New Size(406, 450)
+        Controls.Add(Panel1)
+        Name = "MainCalc"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+
+End Class
---
+++ visualBasic-ProgrammingLanguages-SirTan-BSCS2A/Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class MainCalc
+
+End Class
